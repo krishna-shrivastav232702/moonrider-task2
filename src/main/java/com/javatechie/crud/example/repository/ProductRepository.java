10fa15fde8b1556dd346224d860d748905a299a5
krishna <krishnashrivastava23@gmail.com>
2025-06-21 16:17:10 +0530

    v1.1.0 add product search functionality

--- src/main/java/com/javatechie/crud/example/repository/ProductRepository.java
+++ src/main/java/com/javatechie/crud/example/repository/ProductRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
     Product findByName(String name);
+    List<Product> findByNameContainingIgnoreCase(String keyword);
 }
 
